d11f0e718c8eb0bb208ed44b7122fefb8e73e198
riwu <riwu0730@gmail.com>
2018-09-06 09:04:17 +0800

    add eslint and basic test

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var logger = require('morgan');
+const express = require('express');
+const logger = require('morgan');
 
-var app = express();
+const app = express();
 
-app.use(logger('dev'));
+app.get('/', (req, res) => {
+  res.end(); // for health check
+});
+app.get('/favicon.ico', (req, res) => {
+  res.sendStatus(204); // for browser request
+});
+
+app.use(
+  logger((tokens, req, res) => [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens['response-time'](req, res),
+    'ms',
+    req.headers['x-forwarded-for'],
+    JSON.stringify(req.body),
+  ].join(' ')),
+);
+
 app.use(express.json());
 
 module.exports = app;
